926218016d343429ff267f1d1d077fb0f7fd0b42
Elshafae <Elshfae@example.com>
2023-06-06 13:56:57 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in range(0, 100):
     right_digit = i % 10
     left_digit = int(i / 10) % 10
     if right_digit > left_digit:
-        print("{:0d}, ".format(i), end="")
+        print("{:02d}, ".format(i), end="")
 print()
