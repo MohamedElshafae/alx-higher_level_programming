fbf7231fd4e2668cb854afb5be7776bdc74181d6
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2023-07-11 12:53:55 +0300

    test

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import json
 """ .... """
 
 
 def to_json_string(my_obj):
     """returns the JSON representation of an object (string)"""
+    import json
     data = json.dumps(my_obj)
     return data
