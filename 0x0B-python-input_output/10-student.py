fb2da9f0b0cf9fcb158c2b90cc3ad7d78c263046
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2023-07-12 03:18:27 +0300

    fix_error

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" class """
+"""Student Class"""
 
 
 class Student:
-    """ represent student """
+    """representation of the student"""
     def __init__(self, first_name, last_name, age):
-        """init student"""
+        """init the obj"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None)
+    def to_json(self, attrs=None):
         """class to json"""
         attr_dict = self.__dict__
         if attrs is None:
