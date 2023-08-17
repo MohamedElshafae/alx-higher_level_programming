d387413fc2edaa6bff79ca61d4b03a3e8bedd345
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2023-08-18 01:03:21 +0300

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
 -- lists all privileges of the MySQL users.
-
-
-SHOW GRNATS FOR 'user_0d_1'@'localhost';
-SHOW GRNATS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
