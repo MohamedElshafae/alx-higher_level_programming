84b6d0790782188e93e13bc05f4fd321f4d9c158
Elshafae <maohmedelshafae888@gmail.com>
2023-09-11 18:34:19 +0300

    fix error

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
 
-let i = 0;
-
-process.argv.forEach((val) => {
-  if (i > 1) {
-    console.log(val);
-  }
-  i++;
-});
-
-if (i < 3) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
