2ecf62fe9faed1ce446a04a1e043f4c09e5abd18
Elshafae <maohmedelshafae888@gmail.com>
2023-09-12 20:33:17 +0300

    fix error

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,13 +2,13 @@
 
 const fs = require('fs');
 
-const fileAPath = 'fileA';
-const fileBPath = 'fileB';
+const fileAPath = process.argv[2];
+const fileBPath = process.argv[3];
 
 const fileAContent = fs.readFileSync(fileAPath, 'utf-8');
 const fileBContent = fs.readFileSync(fileBPath, 'utf-8');
 const concatenatedContent = fileAContent + fileBContent;
 
-const concatenatedFilePath = 'fileC';
+const concatenatedFilePath = process.argv[4];
 
 fs.writeFileSync(concatenatedFilePath, concatenatedContent, 'utf-8');
