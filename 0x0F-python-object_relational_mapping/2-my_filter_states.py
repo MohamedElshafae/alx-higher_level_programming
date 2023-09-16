b28b194f4b2720485d9689b916f6e94ab51bd2c4
Elshafae <maohmedelshafae888@gmail.com>
2023-09-16 04:12:24 +0300

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+displays all values in the states table of hbtn_0e_0_usa where name
+"""
+
+import MySQLdb
+import sys
+if __name__ == '__main__':
+    """
+    Access to the database
+    """
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8")
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+    cur.close()
+    conn.close()
