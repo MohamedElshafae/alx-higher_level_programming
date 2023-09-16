400a1439e2b13b8afdb1fefdc4e0d45236a09b6e
Elshafae <maohmedelshafae888@gmail.com>
2023-09-16 04:23:00 +0300

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,12 +16,11 @@ if __name__ == '__main__':
         passwd=sys.argv[2],
         db=sys.argv[3],
         charset="utf8")
-
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}'\
+        ORDER BY id ASC".format(sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
-        if row[1] == sys.argv[4]:
-            print(row)
+        print(row)
     cur.close()
     conn.close()
