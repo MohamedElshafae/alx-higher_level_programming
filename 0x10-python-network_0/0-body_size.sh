22c4fd24096cc2eaabaee665fdae5d22f14f707f
Elshafae <maohmedelshafae888@gmail.com>
2023-09-29 03:19:21 +0300

    try task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL
-curl -sI "$1" | grep Content-Length | gerp -o '[0-9]\+'
+curl -si "$1" | grep Content-Length | gerp -o '[0-9]\+'
