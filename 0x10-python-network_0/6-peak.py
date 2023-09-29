7f6d2052c28034a4e05d6a37ea361bbf9b6be5c8
Elshafae <maohmedelshafae888@gmail.com>
2023-09-29 05:26:31 +0300

    try task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """function that finds a peak in a list of unsorted integers"""
 
+
 def find_peak(list_of_integers):
+    """ finds a peak in a list of unsorted integers."""
     list = sorted(list_of_integers)
     if len(list) == 0:
         return None
