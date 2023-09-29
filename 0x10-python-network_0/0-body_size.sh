7e60aa0da4edfb810d09744db1494918a12d4dbb
Elshafae <maohmedelshafae888@gmail.com>
2023-09-29 03:14:15 +0300

    try task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 #Bash script that takes in a URL, sends a request to that URL
 #and displays the size of the body
 
-curl -sI "$1"| grep 'Content-Length' | gerp -o '[0-9]\+'
+curl -sI "$1"| grep Content-Length | gerp -o '[0-9]\+'
