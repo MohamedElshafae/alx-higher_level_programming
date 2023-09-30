59cbaa816649cb4152dec405997830bd9ddd85bc
Elshafae <maohmedelshafae888@gmail.com>
2023-09-30 03:16:37 +0300

    fix spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         con = response.read()
         print('Body response:')
-        print(f'\t- type:{type(con)}')
-        print(f'\t- content:{con}')
-        print(f'\t- utf8:{con.decode("utf-8")}')
+        print(f'\t- type: {type(con)}')
+        print(f'\t- content: {con}')
+        print(f'\t- utf8: {con.decode("utf-8")}')
