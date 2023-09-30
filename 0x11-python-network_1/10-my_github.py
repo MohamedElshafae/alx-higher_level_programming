7ad5ce855c3aa38c05831906d8794c28c6f8aa8a
Elshafae <maohmedelshafae888@gmail.com>
2023-09-30 05:07:43 +0300

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""doc"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    headers = {'Authorization': f'Bearer {sys.argv[2]}',
+               'X-GitHub-Api-Version': '2022-11-28',
+               'Accept': 'application/vnd.github.v3+json'}
+    url = 'https://api.github.com/user'
+    req = requests.get(url, headers=headers)
+    print(req.json().get('id'))
