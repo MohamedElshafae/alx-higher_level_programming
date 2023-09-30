ee4c76e8c1ba643fa0c05831f2701c90b81a1f0c
Elshafae <maohmedelshafae888@gmail.com>
2023-09-30 04:05:53 +0300

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""doc"""
+
+
+if __name__ == "__main__":
+    from urllib import request, parse
+    from sys import argv
+
+    url = argv[1]
+    data = {
+        'email' : argv[2]
+    }
+    data = parse.urlencode(data).encode('utf-8')
+    with request.urlopen(url) as response:
+        print(response.read().decode('utf-8'))
